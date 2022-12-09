43434e6eddcb56aedccdd08da34e71a1d9328595
zhunor <ozerorhun@gmail.com>
2022-12-09 22:52:38 +0300

    wip

--- scripts/generate_comment.js
+++ scripts/generate_comment.js
@@ -1,10 +1,12 @@
-import openai from 'openai';
+import { Configuration, OpenAIApi } from 'openai';
 
-const openaiClient = new openai.Client({
+const configuration = new Configuration({
   apiKey: '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',
 });
 
-const completions = await openaiClient.completions({
+const openaiClient = new OpenAIApi(configuration);
+
+const completions = await openaiClient.createCompletion({
   model: 'gpt-3',
   prompt:
     'Please review the code changes in this pull request and provide comments on any potential errors or problems you see',
