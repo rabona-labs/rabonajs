a7afd713e51f4e2e90e905be7d8234d7dd2c8ab4
zhunor <ozerorhun@gmail.com>
2022-12-09 23:53:18 +0300

    wip

--- scripts/generate_comment.js
+++ scripts/generate_comment.js
@@ -9,8 +9,7 @@ const openaiClient = new OpenAIApi(configuration);
 try {
   const completions = await openaiClient.createCompletion({
     model: 'text-davinci-003',
-    prompt:
-      'Send me suggestions for these changes in a PR ${{ toJson(steps.git-diff.outputs.stdout) }}',
+    prompt: '${{ toJson(steps.git-diff.outputs.stdout) }}',
     max_tokens: 256,
     n: 1,
   });
