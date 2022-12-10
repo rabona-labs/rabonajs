b1cea5370bb324dc974fa78920dde716e1d1ffcf
zhunor <ozerorhun@gmail.com>
2022-12-10 21:24:30 +0300

    wip

--- scripts/generate_comment.js
+++ scripts/generate_comment.js
@@ -3,6 +3,7 @@ import { Configuration, OpenAIApi } from 'openai';
 const configuration = new Configuration({
   apiKey: '${{ secrets.OPENAI_API_KEY }}',
 });
+
 const openaiClient = new OpenAIApi(configuration);
 
 try {
